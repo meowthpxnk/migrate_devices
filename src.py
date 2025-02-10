efa061086e0926759b7be2c337398523c2102fb8
MeowthPxnk <meowthpxnk@gmail.com>
2025-02-10 13:02:34 +0300

    fix

--- src.py
+++ src.py
@@ -1,6 +1,7 @@
 from run import main as migrate, main_del as delete
 from database_parse import save_map
 from file_export import migrate_auth
+from run import get_connector_servers as test
 
 
 def recreate():
